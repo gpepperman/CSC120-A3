b445ebd02b3c804370316ee76435ce570769ee10
gpepperman <gpepperman@smith.edu>
2025-09-19 11:45:35 -0400

    I have a working bot with inputs and outputs, but no mirroring and no randomness

--- Conversation.java
+++ Conversation.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 class Conversation implements ConversationRequirements {
 
   // Attributes 
-  private List<String> transcript;
-  private Scanner UserInput;
+  private final List<String> transcript;
+  private final Scanner UserInput;
   /**
    * Constructor 
    */
@@ -19,19 +19,23 @@ class Conversation implements ConversationRequirements {
   /**
    * Starts and runs the conversation with the user
    */
-  public void chat(String UserInputs) {
+  public void chat() {
     System.out.println("Start chatting!");
     
-    String input = UserInput.nextLine();
-    transcript.add("User:" + input);
+    for (int i = 0; i < 5; i++){
+      String input = UserInput.nextLine();
+      transcript.add("User:" + input);
 
-    System.out.println("Thanks for saying:" + input);
+      System.out.println("Thanks for saying:" + input);
+    }
+   
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
+    System.out.println("-----Transcript-----");
     for (String line : transcript) {
       System.out.println(line);
     }
@@ -41,12 +45,12 @@ class Conversation implements ConversationRequirements {
    * Gives appropriate response (mirrored or canned) to user input
    * @param inputString the users last line of input
    * @return mirrored or canned response to user input  
-   
+   */
   public String respond(String inputString) {
     String returnString = ""; 
     return returnString; 
   }
-  */
+  
   public static void main(String[] arguments) {
 
     Conversation myConversation = new Conversation();
