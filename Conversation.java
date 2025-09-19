249ff575aa33840bba2fd63cfd15afd35b830aed
gpepperman <gpepperman@smith.edu>
2025-09-19 11:28:43 -0400

    My first shot

--- Conversation.java
+++ Conversation.java
@@ -1,39 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 class Conversation implements ConversationRequirements {
 
   // Attributes 
-
+  private List<String> transcript;
+  private Scanner UserInput;
   /**
    * Constructor 
    */
+  
   Conversation() {
-    
+    this.transcript = new ArrayList<>();
+    this.UserInput = new Scanner(System.in);
   }
 
   /**
    * Starts and runs the conversation with the user
    */
-  public void chat() {
+  public void chat(String UserInputs) {
+    System.out.println("Start chatting!");
+    
+    String input = UserInput.nextLine();
+    transcript.add("User:" + input);
 
+    System.out.println("Thanks for saying:" + input);
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    for (String line : transcript) {
+      System.out.println(line);
+    }
   }
 
   /**
    * Gives appropriate response (mirrored or canned) to user input
    * @param inputString the users last line of input
    * @return mirrored or canned response to user input  
-   */
+   
   public String respond(String inputString) {
     String returnString = ""; 
     return returnString; 
   }
-
+  */
   public static void main(String[] arguments) {
 
     Conversation myConversation = new Conversation();
